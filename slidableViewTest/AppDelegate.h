86f8ce03f2fac4f12ebe34ceb53a9b4dbda60c45
HarveyHu <spot0206@yahoo.com.tw>
2013-08-27 10:12:25 +0800

    Initial Commit

---
+++ slidableViewTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  slidableViewTest
+//
+//  Created by HouKang on 13/8/27.
+//  Copyright (c) 2013年 arplanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
